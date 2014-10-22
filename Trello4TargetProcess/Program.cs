5751ab8b8f89c2a7471e49ee5a04fcc6825c2d7d
George Hahn <george@genericmaker.com>
2014-10-22 18:57:36 -0400

    Add unglyauth code

--- Trello4TargetProcess/Program.cs
+++ Trello4TargetProcess/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,30 +12,64 @@ namespace Trello4TargetProcess
     {
         static void Main(string[] args)
         {
-            if (args.Length < 1)
+            if (args.Length <= 1)
             {
                 Console.WriteLine("Must be caleed with an application key");
                 Console.WriteLine("Get yours at https://trello.com/1/appKey/generate");
+                Console.WriteLine();
+                Console.WriteLine("Usage:");
+                Console.WriteLine("\tRequest authorization:\n\t\t Trello4TargetProcess [appkey] getauth");
+                Console.WriteLine("\tAuthorize:\n\t\t Trello4TargetProcess [appkey] auth [authkey]");
+                Console.WriteLine("\tRun:\n\t\t Trello4TargetProcess [appkey] board [boardid]");
                 return;
             }
 
             var trello = new Trello(args[0]);
+            string boardid;
 
-            if (args.Length > 1)
+            if (args[1] == "getauth")
             {
-                if (args[1].Contains("getauth"))
-                {
-                    var authurl = trello.GetAuthorizationUrl("Trello4TargetProcess", Scope.ReadWrite, Expiration.Never);
-                    Console.WriteLine("Authorization URL recieved");
-                    Console.WriteLine(authurl);
-                }
-                else if (args[1].Contains("auth"))
-                {
-                    trello.Authorize(args[2]);
-                }
+                var authurl = trello.GetAuthorizationUrl("Trello4TargetProcess", Scope.ReadWrite, Expiration.Never);
+                Console.WriteLine("Authorization URL recieved");
+                Console.WriteLine(authurl);
+                return;
             }
 
+            if (args[1] == "auth")
+            {
+                File.WriteAllText("auth.cfg", args[2]);
+                trello.Authorize(args[2]);
+                Console.WriteLine("Successfully authorized");
+                return;
+            }
 
+            if (args[1] == "board")
+            {
+                boardid = args[2];
+            }
+            else
+            {
+                return;
+            }
+
+            if(!File.Exists("auth.cfg"))
+                Console.WriteLine("Not authorized. Run with no arguments to see usage info.");
+
+            trello.Authorize(File.ReadAllText("auth.cfg"));
+
+
+            var board = trello.Boards.WithId(boardid);
+            var lists = trello.Lists.ForBoard(board);
+            var list = lists.First();
+
+            foreach (var card in trello.Cards.ForList(list))
+            {
+                Console.WriteLine(card.Name);
+                Console.WriteLine(card.Id);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
     }
 }
